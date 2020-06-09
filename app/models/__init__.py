46c2755222fb85745096a39777e11fd6c7d7332d
RichardBlackEye <62513655+RichardBlackEye@users.noreply.github.com>
2020-06-10 00:42:46 +0100

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,8 +3,8 @@
 # flake8: noqa
 from __future__ import absolute_import
 # import models into model package
-from swagger_server.models.machine_learning import MachineLearning
-from swagger_server.models.machine_learning_job import MachineLearningJob
-from swagger_server.models.machine_learning_job_placement import MachineLearningJobPlacement
-from swagger_server.models.machine_learning_job_pyspark_job import MachineLearningJobPysparkJob
-from swagger_server.models.machine_learning_job_reference import MachineLearningJobReference
+from app.models.machine_learning import MachineLearning
+from app.models.machine_learning_job import MachineLearningJob
+from app.models.machine_learning_job_placement import MachineLearningJobPlacement
+from app.models.machine_learning_job_pyspark_job import MachineLearningJobPysparkJob
+from app.models.machine_learning_job_reference import MachineLearningJobReference
