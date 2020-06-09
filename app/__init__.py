437d0297c213d56bbcb7292703e5fee21c6a39e0
RichardBlackEye <62513655+RichardBlackEye@users.noreply.github.com>
2020-06-09 21:06:36 +0100

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.machine_learning import MachineLearning
+from swagger_server.models.machine_learning_job import MachineLearningJob
+from swagger_server.models.machine_learning_job_placement import MachineLearningJobPlacement
+from swagger_server.models.machine_learning_job_pyspark_job import MachineLearningJobPysparkJob
+from swagger_server.models.machine_learning_job_reference import MachineLearningJobReference
