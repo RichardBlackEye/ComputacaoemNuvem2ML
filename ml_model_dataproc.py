b5a78f8febf11aacd722b82fb6352aa52f731c79
RichardBlackEye <62513655+RichardBlackEye@users.noreply.github.com>
2020-06-21 21:52:20 +0100

    Add files via upload

---
+++ ml_model_dataproc.py
@@ -0,0 +1,26 @@
+from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.feature import VectorAssembler
+
+
+sc = SparkContext()
+
+spark = SparkSession(sc)
+
+data= spark.read.format("bigquery").option("table", "germany_regression.germany").load()
+
+
+# create features vector
+feature_columns =['NY_ADJ_NNTY_KD_ZG','NY_ADJ_NNTY_KD','NY_ADJ_NNTY_CD','NY_ADJ_NNTY_PC_KD_ZG','NY_ADJ_NNTY_PC_KD','NY_ADJ_NNTY_PC_CD','NY_ADJ_SVNX_GN_ZS','NY_ADJ_SVNX_CD','NY_ADJ_SVNG_GN_ZS'] # here we omit the final column
+
+
+assembler = VectorAssembler(inputCols=feature_columns,outputCol="features")
+
+data_2 = assembler.transform(data)
+
+algo = RandomForestClassifier(featuresCol='features', labelCol='Crise')
+model = algo.fit(data_2) 
+model.save("gs://germl/model")
+
+
